e10adadf544bf0561752b6d50d3329e388134dc7
darren <darren.bridenbeck@gmail.com>
2024-02-04 20:31:21 -0800

    add type to test if it exports right

---
+++ .changeset/thin-balloons-battle.md
@@ -0,0 +1,5 @@
+---
+"darren-b-test-npm-package": patch
+---
+
+add a type to see if it exports correctly
--- src/PerformantContentfulImage/PerformantContentfulImage.tsx
+++ src/PerformantContentfulImage/PerformantContentfulImage.tsx
@@ -1,6 +1,11 @@
 import * as React from "react"
 
-export const PerformantContentfulImage = () => {
+type PerformantContentfulImageType = {
+  whatever?: string
+}
+
+export const PerformantContentfulImage = ({whatever}: PerformantContentfulImageType) => {
+  console.log({whatever})
   return <>
   <p>
     hi it's some stuffs
